d093fc59cdfa4f0961feda5b53ec190b950432fa
Karanjos <85048256+Karanjos@users.noreply.github.com>
2024-04-26 17:16:39 +0530

    complete signup functionality

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import authRoutes from "./routes/authRoute.js";
@@ -16,6 +17,7 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.listen(3000, () => {
   console.log(`Server is running on http://localhost:3000`);
