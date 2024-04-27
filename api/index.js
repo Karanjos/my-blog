153d010256e01e40c27af3e99c771206ae8dbdc8
Karanjos <85048256+Karanjos@users.noreply.github.com>
2024-04-27 12:23:37 +0530

    small changes

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log(`Server is running on http://localhost:3000`);
+  console.log("Server is running on http://localhost:3000");
 });
 
 app.use("/api/user", userRoutes);
