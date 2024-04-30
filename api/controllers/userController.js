6eee56e31e822f593b3f688e91ae8c92679ad9f4
Karanjos <85048256+Karanjos@users.noreply.github.com>
2024-04-30 21:32:26 +0530

    complete user list .

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -2,10 +2,6 @@ import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 import User from "../models/userModel.js";
 
-export const test = (req, res) => {
-  res.json({ message: "All users" });
-};
-
 // update user
 
 export const updateUser = async (req, res, next) => {
@@ -55,7 +51,7 @@ export const updateUser = async (req, res, next) => {
 // delete user
 
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.id) {
+  if (!req.user.isAdmin && req.user.id !== req.params.id) {
     return next(
       errorHandler(401, "You are not authorized to perform this action")
     );
@@ -63,6 +59,45 @@ export const deleteUser = async (req, res, next) => {
   try {
     await User.findByIdAndDelete(req.params.id);
     res.status(200).json({ message: "User has been deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// get all users
+
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(
+      errorHandler(401, "You are not authorized to perform this action")
+    );
+  }
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const userWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+    const totalUsers = await User.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo, $lt: now },
+    });
+    res
+      .status(200)
+      .json({ users: userWithoutPassword, totalUsers, lastMonthUsers });
   } catch (error) {
     next(error);
   }
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { test, updateUser, deleteUser } from "../controllers/userController.js";
+import {
+  updateUser,
+  deleteUser,
+  getUsers,
+} from "../controllers/userController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.get("/", test);
 router.put("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/getusers", verifyToken, getUsers);
+
 
 export default router;
